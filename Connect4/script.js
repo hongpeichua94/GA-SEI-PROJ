f279cf085ebdbc8dc11c6a74a0d746015abe4ffc
hongpeichua <hongggpei@gmail.com>
2024-02-22 19:12:17 +0800

    update document files

--- Connect4/script.js
+++ Connect4/script.js
@@ -10,6 +10,8 @@ let gameOver = false;
 const rows = 6; // y-axis
 const cols = 7; // x-axis
 
+const restartButton = document.getElementById("restart");
+
 // window.onload = function () {
 setGame();
 // };
@@ -24,12 +26,12 @@ function setGame() {
     for (let x = 0; x < cols; x++) {
       row.push(" ");
 
-      // to create <div id="row-col" class="tile"> for 42 tile
+      // to create <div id="row-col" class="tile"> for 42 tiles
       let tile = document.createElement("div");
       tile.id = y.toString() + "-" + x.toString();
       tile.classList.add("tile");
       tile.addEventListener("click", setColumn);
-      document.getElementById("board").append(tile);
+      document.getElementById("game-board").append(tile);
 
       tile.onmouseenter = function () {
         return chooseColumn(x);
@@ -39,13 +41,13 @@ function setGame() {
   }
 }
 
-// drop disc into column / set disc
+// drop disc into column
 function setColumn() {
   if (gameOver) {
     return;
   }
 
-  // get coords of that tile clicked
+  // get coords of tile clicked
   let coords = this.id.split("-");
   let y = parseInt(coords[0]);
   let x = parseInt(coords[1]);
@@ -65,38 +67,142 @@ function setColumn() {
   tile.appendChild(disc);
 
   if (currPlayer == playerOne) {
-    console.log(`Player ${currPlayer}`);
+    // console.log(`Player ${currPlayer}`);
     disc.classList.add("red");
     currPlayer = playerTwo;
   } else {
-    console.log(`Player ${currPlayer}`);
+    // console.log(`Player ${currPlayer}`);
     disc.classList.add("yellow");
     currPlayer = playerOne;
   }
 
-  y -= 1; //updating row height for col
-  currCol[x] = y; //updat currCol array
-  // console.log(y);
+  y -= 1; //updating row height of placed disc
+  currCol[x] = y; //update currCol array
+  console.log(board);
 
   // upate hovering piece
   updateHover();
 
-  // animate dropping pieces
-  // let unplacedDisc = document.querySelector("[data-placed='false']");
-  // let unplacedY = unplacedDisc.getBoundingClientRect().y
-  // let placedY =
+  // animate dropping disc --- styling: disc should drop behind the board - z-axis
+  let unplacedDisc = document.querySelector("[data-placed='false']");
+  let unplacedY = unplacedDisc.getBoundingClientRect().y;
+  let placedY = disc.getBoundingClientRect().y;
+  let yDiff = unplacedY - placedY;
+
+  removeHoverDisc();
+  let animation = disc.animate(
+    [
+      { transform: `translateY(${yDiff}px)`, offset: 0 },
+      { transform: `translateY(0px)`, offset: 0.6 },
+      { transform: `translateY(${yDiff / 20}px)`, offset: 0.8 },
+      { transform: `translateY(0px)`, offset: 0.95 },
+    ],
+    {
+      duration: 400,
+      easing: "linear",
+      iterations: 1,
+    }
+  );
+
+  animation.addEventListener("finish", checkWinner);
 }
 
-function updateHover() {
-  // remove disc when moved away from column
+function checkWinner() {
+  //check for horizontal wins
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols - 3; x++) {
+      if (board[y][x] != " ") {
+        if (
+          board[y][x] == board[y][x + 1] &&
+          board[y][x + 1] == board[y][x + 2] &&
+          board[y][x + 2] == board[y][x + 3]
+        ) {
+          setWinner(y, x);
+          console.log("we have a winner; game ends");
+        }
+      }
+    }
+  }
+
+  //check for vertical wins
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows - 3; y++) {
+      if (board[y][x] != " ") {
+        if (
+          board[y][x] == board[y + 1][x] &&
+          board[y + 1][x] == board[y + 2][x] &&
+          board[y + 2][x] == board[y + 3][x]
+        ) {
+          setWinner(y, x);
+          console.log("we have a winner; game ends");
+        }
+      }
+    }
+  }
+
+  // diagonal bottom left to top right
+  for (let y = 3; y < rows; y++) {
+    for (let x = 0; x < cols - 3; x++) {
+      if (board[y][x] != " ") {
+        if (
+          board[y][x] == board[y - 1][x + 1] &&
+          board[y - 1][x + 1] == board[y - 2][x + 2] &&
+          board[y - 2][x + 2] == board[y - 3][x + 3]
+        ) {
+          setWinner(y, x);
+          console.log("we have a winner; game ends");
+        }
+      }
+    }
+  }
+
+  //diagonal bottom left to top right)
+  for (let y = 0; y < rows - 3; y++) {
+    for (let x = 0; x < cols - 3; x++) {
+      if (board[y][x] != " ") {
+        if (
+          board[y][x] == board[y + 1][x + 1] &&
+          board[y + 1][x + 1] == board[y + 2][x + 2] &&
+          board[y + 2][x + 2] == board[y + 3][x + 3]
+        ) {
+          setWinner(y, x);
+          console.log("we have a winner; game ends");
+        }
+      }
+    }
+  }
+}
+
+function setWinner(y, x) {
+  // let winner = document.getElementById("winner");
+  const alertContainer = document.getElementById("alert-container");
+  const alertMessage = document.getElementById("message");
+  if (board[y][x] == playerOne) {
+    // winner.innerText = "Player 1 Wins";
+    alertContainer.style.display = "block";
+    alertMessage.innerText = "Player 1 Wins!!";
+  } else {
+    // winner.innerText = "Player 2 Wins";
+    alertContainer.style.display = "block";
+    alertMessage.innerText = "Player 2 Wins!!";
+  }
+  gameOver = true;
+}
+
+// remove disc when moved away from column
+function removeHoverDisc() {
   let unplacedDisc = document.querySelector("[data-placed='false']");
   if (unplacedDisc) {
     unplacedDisc.parentElement.removeChild(unplacedDisc);
   }
+}
+
+function updateHover() {
+  removeHoverDisc();
 
   // add disc when column is not full
   if (currCol[hoverColumn] > -1) {
-    let tile = document.getElementById("board").children[hoverColumn];
+    let tile = document.getElementById("game-board").children[hoverColumn];
     let disc = document.createElement("div");
     disc.classList.add("disc");
     disc.dataset.placed = false;
@@ -110,3 +216,13 @@ function chooseColumn(col) {
   hoverColumn = col;
   updateHover();
 }
+
+// reset the board and tiles
+function resetGame() {
+  location.reload();
+}
+
+restartButton.addEventListener("click", resetGame);
+
+// function checkDraw()
+// timer()
