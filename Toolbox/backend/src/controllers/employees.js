90e8d8b684ee87e5716002c1e0e4bc518a5d444d
hongpeichua <hongggpei@gmail.com>
2024-04-24 11:27:18 +0800

    Update employees

--- Toolbox/backend/src/controllers/employees.js
+++ Toolbox/backend/src/controllers/employees.js
@@ -109,8 +109,104 @@ const updateEmployeeDetails = async (req, res) => {
   }
 };
 
+const addEmployeeTitles = async (req, res) => {
+  try {
+    // Retrieve the employee id based on accounts email
+    const employeeResult = await db.query(
+      "SELECT id FROM employees JOIN accounts ON employees.account_id = accounts.uuid WHERE accounts.email = $1",
+      [req.body.email]
+    );
+
+    if (employeeResult.rows.length === 0) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Employee does not exist" });
+    }
+
+    const employeeId = employeeResult.rows[0].id;
+
+    // Retrieve the department_id based on the department name
+    const departmentResult = await db.query(
+      "SELECT id FROM departments WHERE name = $1",
+      [req.body.department]
+    );
+
+    if (departmentResult.rows.length === 0) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Department does not exist" });
+    }
+
+    const departmentId = departmentResult.rows[0].id;
+
+    await db.query(
+      `INSERT INTO employee_titles (employee_id, title, start_date, department_id, status, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, NOW(), NOW())`,
+      [employeeId, req.body.title, req.body.start_date, departmentId, "ACTIVE"]
+    );
+    res.json({ status: "ok", msg: "Employee titles created successfully" });
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(400)
+      .json({ status: "error", msg: "Error adding new employee titles" });
+  }
+};
+
+// Update employee titles by uuid (params)
+const updateEmployeeTitles = async (req, res) => {
+  try {
+    const updateQuery = `
+      UPDATE employee_titles 
+      SET 
+        status = $1,
+        end_date = $2,
+        updated_at = NOW()
+      WHERE 
+        uuid = $3`;
+
+    // Execute the update query with parameters
+    await db.query(updateQuery, [
+      req.body.status,
+      req.body.end_date,
+      req.params.uuid,
+    ]);
+
+    res
+      .status(200)
+      .json({ status: "success", msg: "Employee title updated successfully" });
+  } catch (error) {
+    console.error("Error updating employee title:", error);
+    res.status(500).json({ status: "error", msg: "Internal server error" });
+  }
+};
+
+// Search by work email > return employee id
+const getEmployeeTitlesByEmail = async (req, res) => {
+  try {
+    let query = `SELECT employee_titles.* FROM employees JOIN employee_titles on employees.id = employee_titles.employee_id JOIN accounts on employees.account_id = accounts.uuid WHERE 1 = 1`;
+
+    const values = [];
+
+    if (req.query.input !== undefined) {
+      query += " AND accounts.email ilike '%' || $1 || '%'";
+      values.push(req.query.input);
+    }
+
+    const result = await db.query(query, values);
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(400)
+      .json({ status: "error", msg: "Error getting employee titles" });
+  }
+};
+
 module.exports = {
   getAllEmployees,
   getEmployeeByEmailOrName,
   updateEmployeeDetails,
+  addEmployeeTitles,
+  updateEmployeeTitles,
+  getEmployeeTitlesByEmail,
 };
--- Toolbox/backend/src/routers/employees.js
+++ Toolbox/backend/src/routers/employees.js
@@ -3,6 +3,9 @@ const {
   getAllEmployees,
   getEmployeeByEmailOrName,
   updateEmployeeDetails,
+  addEmployeeTitles,
+  updateEmployeeTitles,
+  getEmployeeTitlesByEmail,
 } = require("../controllers/employees");
 
 const router = express.Router();
@@ -10,5 +13,8 @@ const router = express.Router();
 router.get("/employees", getAllEmployees);
 router.get("/employees/search", getEmployeeByEmailOrName);
 router.patch("/employee/:account_id", updateEmployeeDetails);
+router.put("/employee/titles", addEmployeeTitles);
+router.patch("/employee/titles/:uuid", updateEmployeeTitles);
+router.get("/employee/titles/search", getEmployeeTitlesByEmail);
 
 module.exports = router;
