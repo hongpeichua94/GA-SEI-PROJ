65afcd6ce239684271d6ce2331dfaef223fd6fe6
hongpeichua <hongggpei@gmail.com>
2024-04-26 09:50:46 +0800

    Create FE starter

--- Toolbox/frontend/src/App.jsx
+++ Toolbox/frontend/src/App.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import UserContext from "./context/user";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const [accountId, setAccountId] = useState("");
+  const [role, setRole] = useState("");
+
+  const userContextValue = {
+    accessToken,
+    setAccessToken,
+    accountId,
+    setAccountId,
+    role,
+    setRole,
+  };
+
+  //converting accessToken value to a boolean to see if user is logged in
+  const isLoggedIn = !!accessToken;
+
   return (
-    <div>
-      <h2>GA SEI</h2>
-    </div>
+    <UserContext.Provider value={userContextValue}>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route
+          path="login"
+          element={isLoggedIn ? <Navigate to="/dashboard" /> : <Login />}
+        />
+        <Route
+          path="dashboard"
+          element={
+            isLoggedIn ? (
+              <Dashboard accountId={accountId} />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </UserContext.Provider>
   );
 }
 
