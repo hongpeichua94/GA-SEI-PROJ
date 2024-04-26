ec581b5ab3860b0eb1cbed160d7b712bf5050d0c
hongpeichua <hongggpei@gmail.com>
2024-04-26 09:51:39 +0800

    Create Login.jsx

---
+++ Toolbox/frontend/src/pages/Login.jsx
@@ -0,0 +1,108 @@
+import React, { useContext, useState } from "react";
+import useFetch from "../hooks/useFetch";
+import UserContext from "../context/user";
+
+import { ToolTwoTone } from "@ant-design/icons";
+import { Button, Form, Input, Typography } from "antd";
+
+const Login = (props) => {
+  const { Title } = Typography;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const fetchData = useFetch();
+
+  const userCtx = useContext(UserContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await fetchData("/auth/login", "POST", { email, password });
+
+    if (res.ok) {
+      userCtx.setAccessToken(res.data.access);
+      const decoded = jwtDecode(res.data.access);
+      userCtx.setRole(decoded.role);
+    } else {
+      alert(JSON.stringify(res.data));
+    }
+  };
+
+  return (
+    <>
+      <div className="login-container">
+        <Title style={{ textAlign: "center" }}>
+          Hi, Welcome to Toolbox
+          <ToolTwoTone />
+        </Title>
+        <div className="login-form">
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            >
+              <Input.Password />
+            </Form.Item>
+            <br />
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit" onClick={handleLogin}>
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Login;
