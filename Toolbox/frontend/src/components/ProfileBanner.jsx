f5dec2a651b134faed788f02cccb57c3df782d44
hongpeichua <hongggpei@gmail.com>
2024-04-28 01:12:17 +0800

    Create ProfileBanner.jsx

---
+++ Toolbox/frontend/src/components/ProfileBanner.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+// ANT DESIGN
+import { Layout, theme, Avatar, Space } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+import styles from "../pages/Profile.module.css";
+
+const { Header } = Layout;
+
+const ProfileBanner = (props) => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Header
+      style={{
+        padding: 10,
+        margin: "10px 16px",
+        height: 120,
+        maxWidth: "90vw",
+        background: colorBgContainer,
+        borderRadius: borderRadiusLG,
+      }}
+    >
+      <Space wrap size={16} className={styles.banner}>
+        <div className={styles.userInfo}>
+          <Avatar size={90} icon={<UserOutlined />} src={props.profilePic} />
+
+          <div className={styles.about}>
+            <h4>
+              {props.firstName} {props.lastName}{" "}
+            </h4>
+            <p>{props.title}</p>
+          </div>
+        </div>
+
+        <div className={styles.joinedDate}>
+          <h5>Joined since</h5>
+          <h4>{props.joinedDate}</h4>
+        </div>
+      </Space>
+    </Header>
+  );
+};
+
+export default ProfileBanner;
